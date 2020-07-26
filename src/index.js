6c47092b215f5a949598d7d90dc30417b6c00b39
Chris King <cking5836@gmail.com>
2020-07-25 21:42:14 -0400

    Redux set up and all needed dependencies are installed. Folders are set up for components, reducers and actions.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer';
+
+const store = createStore(reducer); // Right now reducer has no real shape
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/reducer.js
@@ -0,0 +1,6 @@
+
+
+// This is the intial file for our reducer functions
+export function reducer(){
+    return null
+}
