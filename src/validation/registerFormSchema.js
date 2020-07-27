d3178cfd50d0285664692ddee8ad10a9f7a63a25
Zachary Peterson <zp.lambda@yahoo.com>
2020-07-27 16:20:01 -0400

    Sign-up form

---
+++ src/validation/registerFormSchema.js
@@ -0,0 +1,30 @@
+import * as yup from 'yup'
+
+
+const registerFormSchema = yup.object().shape({
+fname: yup.string()
+    .trim()   
+    .min(2, "First name must be at least 2 characters")
+    .required("Username is required"),
+lname: yup.string()
+    .trim()   
+    .min(2, "Last name must be at least 2 characters")
+    .required("Username is required"),
+username: yup.string()
+    .trim()   
+    .min(4, "Username must be at least 4 characters")
+    .required("Username is required"),
+password: yup.string()   
+    .trim()
+    .min(4, 'Password must be at least 4 characters')    
+    .required("Password is Required"),
+email: yup.string()
+    .trim()   
+    .min(4, "Email must be at least 4 characters")
+    .required("Username is required"),
+location : yup.string()
+    .trim()   
+    .min(4, "Location must be at least 4 characters"),
+      
+});
+export default registerFormSchema;
