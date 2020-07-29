e247311d78282b38105665b98b5355bde502784d
Stacey Louis <stacey-louis@lambdastudents.com>
2020-07-29 13:01:01 -0400

    added post component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './components/login/Login';
 import { Switch, Route, useHistory, NavLink, Link} from 'react-router-dom'
 import Register from './components/sign-up/Register'
 import { connect } from 'react-redux'
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import { fetchData } from './action/action'
 import { fetchPosts } from './action/fetchPosts';
 
--- src/components/Dashboard/addPost.js
+++ src/components/Dashboard/addPost.js
@@ -3,9 +3,19 @@ import React from 'react'
 function addPost() {
 
     return(
-        <div></div>
+        <div>
+<form id="addPost">
+    <h1> Add Post </h1>
+    <input 
+    type="text"
+    value=""
+    onChange=""
+     />
+</form>
+
+        </div>
     )
 
 }
 
-export default addPost
+export default addPost;
