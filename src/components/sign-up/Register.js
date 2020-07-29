288e15ec26482ad9110e2cfe200e9539d4bdeeb9
Chris King <cking5836@gmail.com>
2020-07-28 21:09:27 -0400

    Login and registration are both workinggit add .

--- src/components/sign-up/Register.js
+++ src/components/sign-up/Register.js
@@ -12,13 +12,16 @@ import { Link, useHistory } from 'react-router-dom';
 const initialVal = {
     name: '',
     username:'',
+    email: '',
+    location: ''
 }
 
 const initialErrors = {
 
     name: '',
     username: '',
-
+    email: '',
+    location: ''
 };
 
 const StyledContainer = styled.form`
@@ -168,7 +171,7 @@ function Register(props){
             /></td>
         </tr>
 
-        {/* <tr>
+        <tr>
             <td><label htmlFor='email'>Email:</label></td>
             <td><input
                 name='email'
@@ -177,67 +180,19 @@ function Register(props){
                 type='email'
                 placeholder='Please enter an email'
             /></td>
-        </tr> */}
+        </tr>
 
-        {/* <tr>
+        <tr>
             <td><label htmlFor='location'>Location:</label></td>
-            <td>
-                <select>
-                    <option>Please select a location</option>
-                    <option value="AL">Alabama</option>
-                    <option value="AK">Alaska</option>
-                    <option value="AZ">Arizona</option>
-                    <option value="AR">Arkansas</option>
-                    <option value="CA">California</option>
-                    <option value="CO">Colorado</option>
-                    <option value="CT">Connecticut</option>
-                    <option value="DE">Delaware</option>
-                    <option value="DC">District Of Columbia</option>
-                    <option value="FL">Florida</option>
-                    <option value="GA">Georgia</option>
-                    <option value="HI">Hawaii</option>
-                    <option value="ID">Idaho</option>
-                    <option value="IL">Illinois</option>
-                    <option value="IN">Indiana</option>
-                    <option value="IA">Iowa</option>
-                    <option value="KS">Kansas</option>
-                    <option value="KY">Kentucky</option>
-                    <option value="LA">Louisiana</option>
-                    <option value="ME">Maine</option>
-                    <option value="MD">Maryland</option>
-                    <option value="MA">Massachusetts</option>
-                    <option value="MI">Michigan</option>
-                    <option value="MN">Minnesota</option>
-                    <option value="MS">Mississippi</option>
-                    <option value="MO">Missouri</option>
-                    <option value="MT">Montana</option>
-                    <option value="NE">Nebraska</option>
-                    <option value="NV">Nevada</option>
-                    <option value="NH">New Hampshire</option>
-                    <option value="NJ">New Jersey</option>
-                    <option value="NM">New Mexico</option>
-                    <option value="NY">New York</option>
-                    <option value="NC">North Carolina</option>
-                    <option value="ND">North Dakota</option>
-                    <option value="OH">Ohio</option>
-                    <option value="OK">Oklahoma</option>
-                    <option value="OR">Oregon</option>
-                    <option value="PA">Pennsylvania</option>
-                    <option value="RI">Rhode Island</option>
-                    <option value="SC">South Carolina</option>
-                    <option value="SD">South Dakota</option>
-                    <option value="TN">Tennessee</option>
-                    <option value="TX">Texas</option>
-                    <option value="UT">Utah</option>
-                    <option value="VT">Vermont</option>
-                    <option value="VA">Virginia</option>
-                    <option value="WA">Washington</option>
-                    <option value="WV">West Virginia</option>
-                    <option value="WI">Wisconsin</option>
-                    <option value="WY">Wyoming</option>
-                </select>
-            </td>
-        </tr> */}
+            <td><input
+                name='location'
+                onChange={onInputChange}
+                type='text'
+                placeholder='Please enter a location'
+            /></td>
+        </tr>
+
+        
 
         </table> 
         
@@ -246,8 +201,8 @@ function Register(props){
             <p>{formErrors.name}</p>
             <p>{formErrors.username}</p>
             <p>{formErrors.password}</p>
-            {/* <p>{formErrors.email}</p>
-            <p>{formErrors.location}</p>   */}
+            <p>{formErrors.email}</p>
+            {/* <p>{formErrors.location}</p>   */}
         </div>
         
 
