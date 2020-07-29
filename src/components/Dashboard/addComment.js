8fe155d80c5b848a84be2ba37de10f5eafcee9f8
Zachary Peterson <zp.lambda@yahoo.com>
2020-07-29 14:36:23 -0400

    Needs check

--- src/components/Dashboard/addComment.js
+++ src/components/Dashboard/addComment.js
@@ -1,21 +1,51 @@
 import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../../utils/axiosWithAuth';
+import axios from 'axios'
+import { fetchData } from '../../action/action'
+import { connect } from 'react-redux';
 
-const addComment = (props) => {
+const AddComment = (props) => {
+
+    // const [comment, setComment] =useState([]);
+
+    // const newCommentSubmit = e => {
+    //     e.preventDefault()
+    //     axiosWithAuth()
+    //     .post(`https://expat-journal-web31.herokuapp.com/api/posts/:id/comments`, formValues)
+    //     .then(res => {
+    //         setComment(res.data)
+    //         console.log(res.data)
+    //     })
+    //     .catch(err =>{
+    //         console.log(err)
+    //     })
+
+    // }
+
+    // const onInputChange = e => {
+    //     const { name, value } = e.target
+    //      setComment({
+    //          ...comment,
+    //          [name]: value
+    //      })
+    //     }
 
     return(
         <div>
 
             <form id="addComment">
                 <input 
-                    onChange=''
-                    placeholder={`Post a comment as ${data.username}...`}
+                    // onChange={onInputChange}
+                    placeholder={`Post a comment as ${props.username}...`}
                     type='text'
                     value=''
                 />
+
+                {/* <button onClick={newCommentSubmit} >Submit</button> */}
             </form>
 
         </div>
-    )
-}
+    );
+};
 
-export default addComment;
+export default AddComment;
