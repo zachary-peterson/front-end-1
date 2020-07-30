7145f215704a9c849307cb8a3022ee78f229c94b
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-07-30 11:56:23 -0400

    wrap private route to dashboard and inprocess of update and deleted post

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,5 @@
 import { FETCH_DATA_START, FETCH_DATA_SUCCESS } from '../action/action'
+import { FETCH_POSTDATA_START, FETCH_POSTDATA_SUCCESS } from '../action/fetchPosts'
 
 // This is the intial file for our reducer functions
 // export function reducer(){
@@ -8,6 +9,7 @@ import { FETCH_DATA_START, FETCH_DATA_SUCCESS } from '../action/action'
 const initialState = {
     loading: false,
     user: [],
+    posts:[],
     error: "",
 
 }
@@ -25,6 +27,18 @@ export const reducer = ( state = initialState, action ) => {
                 user : action.payload,
                 error: ""
             }    
+            case FETCH_POSTDATA_START:
+                return {
+                    ...state,
+                    loading: true
+                };
+            case FETCH_POSTDATA_SUCCESS:
+                return {
+                    ...state,
+                    loading: false,
+                    posts : action.payload,
+                    error: ""
+                }
         
         default: 
         return state;
